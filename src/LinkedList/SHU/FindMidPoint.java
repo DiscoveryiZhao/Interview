3bed0c7ef2d7f52d8cffc8200b731fc39512d527
Yi Zhao <yizhao.cs@gmail.com>
2015-01-18 17:20:44 -0800

    1

--- src/LinkedList/SHU/FindMidPoint.java
+++ src/LinkedList/SHU/FindMidPoint.java
@@ -1,4 +1,4 @@
-package LinkedList;
+package LinkedList.SHU;
 
 import LeetcodeLibrary.ListNode;
 
---
+++ src/LinkedList/SHU/Insertion.java
@@ -0,0 +1,7 @@
+package LinkedList.SHU;
+
+/**
+ * Created by yizhao on 1/18/15.
+ */
+public class Insertion {
+}
