41b871aded5e97ebb7cbdc2311bedd33e955388c
Yi Zhao <yizhao.cs@gmail.com>
2015-01-18 16:51:18 -0800

    1

---
+++ src/HashArrayTable/HashMap/MajorityElement.java
@@ -0,0 +1,34 @@
+package HashArrayTable.HashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by yizhao on 12/12/14.
+ */
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] num1 = {1};
+        System.out.println(majorityElement(num1)); // 1
+        int[] num2 = {1, 2, 3, 2, 2};
+        System.out.println(majorityElement(num2)); // 2
+        int[] num3 = {3, 1, 2, 3, 2, 3, 3};
+        System.out.println(majorityElement(num3)); // 3
+    }
+
+    public static int majorityElement(int[] num) {
+        int n = num.length;
+        Map<Integer, Integer> hash = new HashMap<>();
+        for (int i : num) {
+            if (hash.containsKey(i)) {
+                hash.put(i, hash.get(i) + 1);
+            } else {
+                hash.put(i, 1);
+            }
+            if (hash.get(i) > n / 2) {
+                return i;
+            }
+        }
+        return num[0];
+    }
+}
---
+++ src/Recursion/TriangleNumber.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+/**
+ * Created by yizhao on 1/3/15.
+ */
+public class TriangleNumber {
+    public static void main(String[] args) {
+        System.out.println(recur(0)); // 0
+        System.out.println(recur(1)); // 1
+        System.out.println(recur(2)); // 3
+        System.out.println(recur(3)); // 6
+        System.out.println(recur(4)); // 10
+    }
+
+    public static int recur(int n){
+        if(n==0||n==1){
+            return n;
+        }else{
+            return recur(n-1)+n;
+        }
+    }
+}
