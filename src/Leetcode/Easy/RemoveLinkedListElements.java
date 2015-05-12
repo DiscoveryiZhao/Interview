f0cca6bb2d62ac997f0003b31df0eb80d6c47b93
Yi Zhao <yizhao.cs@gmail.com>
2015-05-12 14:23:28 -0700

    1

--- src/Leetcode/Easy/RemoveLinkedListElements.java
+++ src/Leetcode/Easy/RemoveLinkedListElements.java
@@ -7,26 +7,43 @@ import Leetcode.Library.ListNode;
  */
 public class RemoveLinkedListElements {
     public static void main(String[] args) {
+        ListNode test1 = new ListNode(1);
+        test1.next = new ListNode(2);
+        ListNode res1 = removeElements(test1, 1);
+        while(res1 != null){
+            System.out.print(res1.val);
+            System.out.print("->");
+            res1 = res1.next;
+        }
 
+        System.out.println();
+
+        ListNode test2 = new ListNode(1);
+        test2.next = new ListNode(1);
+        ListNode res2 = removeElements(test2, 1);
+        while(res2 != null){
+            System.out.print(res2.val);
+            System.out.print("->");
+            res2 = res2.next;
+        }
     }
 
     public static ListNode removeElements(ListNode head, int val) {
         if (head == null) return null;
-        if (head.val == val) {
-            return null;
-        } else {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
 
-            ListNode cur = head;
-            while (cur != null && cur.next != null) {
-                ListNode nt = cur.next;
-                if (nt.val == val) {
-                    cur.next = nt.next;
-                } else {
-                    cur = cur.next;
-                }
-
+        ListNode pre = dummy;
+        ListNode cur = head;
+        while(cur!=null){
+            if(cur.val == val){
+                pre.next = pre.next.next;
+                break;
             }
-            return head;
+            cur = cur.next;
+            pre = pre.next;
         }
+
+        return dummy.next;
     }
 }
