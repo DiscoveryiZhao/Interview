450cc586de7a2411f9895741e2ae5c24ef8def68
Yi Zhao <yizhao.cs@gmail.com>
2015-05-13 14:58:32 -0700

    1

---
+++ src/Leetcode/Medium/MaximumProductSubarray.java
@@ -0,0 +1,36 @@
+package Leetcode.Medium;
+
+/*
+Find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+For example, given the array [2,3,-2,4],
+the contiguous subarray [2,3] has the largest product = 6.
+*/
+public class MaximumProductSubarray {
+    public static void main(String[] args){
+        int[] nums = {2, 3, -2, 4};
+        System.out.println(maxProduct(nums)); // 6
+    }
+
+    public static int maxProduct(int[] nums) {
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
+
+        min[0] = max[0] = nums[0];
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            min[i] = max[i] = nums[i];
+            if (nums[i] > 0) {
+                max[i] = Math.max(max[i], max[i - 1] * nums[i]);
+                min[i] = Math.min(min[i], min[i - 1] * nums[i]);
+            } else if (nums[i] < 0) {
+                max[i] = Math.max(max[i], min[i - 1] * nums[i]);
+                min[i] = Math.min(min[i], max[i - 1] * nums[i]);
+            }
+
+            result = Math.max(result, max[i]);
+        }
+
+        return result;
+    }
+}
