4e2cb69705734b2398ea207e4bcd7aba5ccf85f6
Yi Zhao <yizhao.cs@gmail.com>
2015-05-19 17:13:49 -0700

    1

--- src/LinkedList/SHU/Deletion.java
+++
@@ -1,7 +0,0 @@
-package LinkedList.SHU;
-
-/**
- * Created by yizhao on 1/18/15.
- */
-public class Deletion {
-}
---
+++ src/LinkedList/SHU/RemoveNodeFromLinkedList.java
@@ -0,0 +1,27 @@
+package LinkedList.SHU;
+
+import Leetcode.Library.ListNode;
+
+/**
+ * Created by yizhao on 1/18/15.
+ */
+public class RemoveNodeFromLinkedList {
+
+    public ListNode removeNodeFromLinkedList(ListNode head, int target){
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode preHead = dummy;
+
+        while(head!=null){
+            if(head.val == target){
+                preHead.next = preHead.next.next;
+                break;
+            }
+            head = head.next;
+            preHead = preHead.next;
+        }
+
+        return dummy.next;
+    }
+}
