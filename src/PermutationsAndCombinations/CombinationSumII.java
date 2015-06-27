6e81f263dc6f02ede693915073c437bca09fa5e6
Yi Zhao <yizhao.cs@gmail.com>
2015-06-27 15:38:33 -0700

    1

--- src/PermutationsAndCombinations/CombinationSumII.java
+++ src/PermutationsAndCombinations/CombinationSumII.java
@@ -17,22 +17,22 @@ public class CombinationSumII {
     public static List<List<Integer>> combinationSum2(int[] num, int sum) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(num);// 去重
-        helper(num, sum, res, new ArrayList<Integer>(), 0);
+        vector(num, sum, res, new ArrayList<Integer>(), 0);
         return res;
     }
 
-    public static void helper(int[] num, int sum, List<List<Integer>> res, List<Integer> tmp, int depth) {
+    public static void vector(int[] num, int sum, List<List<Integer>> res, List<Integer> tmp, int depth) {
         if (sum <= 0) {
             if (sum == 0) {
                 res.add(new ArrayList<>(tmp)); // 什么时候输出结果
             }
             return;
         }
-        for (int i = depth; i < num.length; i++) {
-            if (i != depth && num[i] == num[i - 1]) continue; // 什么情况跳过
-            tmp.add(num[i]);
+        for (int width = depth; width < num.length; width++) {
+            if (width != depth && num[width] == num[width - 1]) continue; // 什么情况跳过
+            tmp.add(num[width]);
             // 这里pass入i+1而不是i进recursion，不同于Combination Sum那题，是因为一个数只能用一次
-            helper(num, sum - num[i], res, tmp, i + 1);
+            vector(num, sum - num[width], res, tmp, width + 1);
             tmp.remove(tmp.size() - 1);
         }
     }
