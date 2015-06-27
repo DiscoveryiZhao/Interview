82c3d2de22c3022319e35e19f84eac9895ce98de
Yi Zhao <yizhao.cs@gmail.com>
2015-06-27 11:04:14 -0700

    1

--- src/PermutationsAndCombinations/SubsetsII.java
+++ src/PermutationsAndCombinations/SubsetsII.java
@@ -25,12 +25,7 @@ public class SubsetsII {
     public static void vector(int[] S, List<List<Integer>> res, List<Integer> tmp, int depth) {
         res.add(new ArrayList<>(tmp)); // 什么时候输出结果
         for (int width = depth; width < S.length; width++) {
-            if (width != depth && S[width] == S[width - 1]) {
-                System.out.println("i:" + width + "  ,depth:"  + depth + "  ,S[i]:" + S[width] + "  ,tmp:" + tmp);
-                // i:2  ,depth:1  ,S[i]:2  ,tmp:[1]
-                // i:2  ,depth:0  ,S[i]:2  ,tmp:[]
-                continue; // 什么情况跳过
-            }
+            if (width != depth && S[width] == S[width - 1]) continue; // 什么情况跳过
             tmp.add(S[width]);
             vector(S, res, tmp, width + 1);
             tmp.remove(tmp.size() - 1);
