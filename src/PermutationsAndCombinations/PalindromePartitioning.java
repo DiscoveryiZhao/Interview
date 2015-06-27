4ba50ec5d6a808ea2ccded4fdfdbcd2223d1ca88
Yi Zhao <yizhao.cs@gmail.com>
2015-06-26 20:24:56 -0700

    1

---
+++ src/PermutationsAndCombinations/PalindromePartitioning.java
@@ -0,0 +1,47 @@
+package PermutationsAndCombinations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yizhao on 6/26/15.
+ */
+public class PalindromePartitioning {
+    public static void main(String[] args) {
+        // [[a, a, b, s, d, s, a], [a, a, b, sds, a], [aa, b, s, d, s, a], [ aa, b, sds, a]]
+        System.out.println(partition("aabsdsa"));
+    }
+
+    public static List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+        helper(s, res, new ArrayList<String>());
+        return res;
+    }
+
+    public static void helper(String s, List<List<String>> res, List<String> tmp) {
+        int n = s.length();
+        if (n == 0) {
+            res.add(new ArrayList<String>(tmp));
+            return;
+        }
+        for (int i = 1; i <= n; i++) {
+            String sofar = s.substring(0, i);
+            if (isPalindrome(sofar)) {
+                tmp.add(sofar);
+                helper(s.substring(i), res, tmp);
+                tmp.remove(tmp.size() - 1);
+            }
+        }
+    }
+
+    public static boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i++) != s.charAt(j--))
+                return false;
+        }
+        return true;
+    }
+
+}
---
+++ src/PermutationsAndCombinations/WordBreakII.java
@@ -0,0 +1,63 @@
+package PermutationsAndCombinations;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Created by yizhao on 6/26/15.
+ */
+public class WordBreakII {
+    public static void main(String[] args) {
+        Set<String> test9 = new HashSet<String>();
+
+        test9.add( "cat");
+        test9.add( "cats");
+        test9.add( "and");
+        test9.add( "sand");
+        test9.add( "dog");
+        System. out.println(wordBreak( "catsanddog", test9)); // [cat sand dog, cats and dog]
+
+        String[] tmp = { "a", "aa" , "aaa" , "aaaa" , "aaaaa" , "aaaaaa" , "aaaaaaa" , "aaaaaaaa", "aaaaaaaaa" , "aaaaaaaaaa" };
+        Set<String> test10 = new HashSet<String>();
+        for (String s : tmp) {
+            test10.add(s);
+        }
+
+        System. out.println(wordBreak( "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab" , test10)); // true
+    }
+
+    public static List<String> wordBreak(String s, Set<String> dict) {
+        int n = s.length();
+        ArrayList<String> res = new ArrayList<String>();
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true; // case for s = ""
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] = dp[j] && dict.contains(s.substring(j, i)) ? true : dp[i];
+            }
+        }
+        // This problem can not be solve because dp[n] is false
+        if (dp[n] == false) {
+            return res;
+        }
+        helper(s, dict, res, "");
+        return res;
+    }
+
+    private static void helper(String s, Set<String> dict, ArrayList<String> res, String tmp) {
+        int n = s.length();
+        if (n == 0) {
+            // tmp.length() - 1 use to remove the last " " in tmp String.
+            res.add(tmp.substring(0, tmp.length() - 1));
+        } else {
+            for (int i = 1; i <= n; i++) {
+                String soFar = s.substring(0, i);
+                if (dict.contains(soFar)) {
+                    helper(s.substring(i), dict, res, tmp + soFar + " ");
+                }
+            }
+        }
+    }
+}
