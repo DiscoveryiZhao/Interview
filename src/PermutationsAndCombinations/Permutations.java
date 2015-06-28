8af4ae464b76c1be2a2599511c2157f3fb670edb
Yi Zhao <yizhao.cs@gmail.com>
2015-06-28 10:24:51 -0700

    1

--- src/PermutationsAndCombinations/Permutations.java
+++ src/PermutationsAndCombinations/Permutations.java
@@ -9,25 +9,27 @@ import java.util.List;
 public class Permutations {
     public static void main(String[] args) {
         int[] S = {1, 2, 3};
-        // [[], [1], [1, 2], [1, 2, 2], [2], [2, 2]]
+        // [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]
         System.out.println(permute(S));
     }
     public static List<List<Integer>> permute(int[] num) {
         List<List<Integer>> res = new ArrayList<>();
-        vector(num, res, new ArrayList<Integer>());
+        vector(num, res, new ArrayList<Integer>(), new boolean[num.length]);
         return res;
     }
 
-    public static void vector(int[] num, List<List<Integer>> res, List<Integer> tmp) {
+    public static void vector(int[] num, List<List<Integer>> res, List<Integer> tmp, boolean[] visit) {
         if (tmp.size() == num.length) {
             res.add(new ArrayList<>(tmp)); // 什么时候输出结果
             return;
         }
         for (int width = 0; width < num.length; width++) {
-            if (tmp.contains(num[width])) continue; // 什么情况跳过
+            if (visit[width] == true) continue;  // 什么情况跳过
+            visit[width] = true;
             tmp.add(num[width]);
-            vector(num, res, tmp);
+            vector(num, res, tmp, visit);
             tmp.remove(tmp.size() - 1);
+            visit[width] = false;
         }
     }
 }
