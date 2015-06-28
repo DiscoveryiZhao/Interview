2a8492e793fdd64f90f2f8ef8238df414c99c7c2
Yi Zhao <yizhao.cs@gmail.com>
2015-06-27 19:58:54 -0700

    1

--- src/PermutationsAndCombinations/LetterCombinationsofaPhoneNumber.java
+++ src/PermutationsAndCombinations/LetterCombinationsofaPhoneNumber.java
@@ -9,14 +9,19 @@ import java.util.Map;
  * Created by yizhao on 6/26/15.
  */
 public class LetterCombinationsofaPhoneNumber {
-    public List<String> letterCombinations(String digits) {
+    public static void main(String[] args) {
+        // [adg , adh, adi, aeg, aeh, aei , afg, afh, afi, bdg, bdh , bdi, beg, beh, bei, bfg , bfh, bfi, cdg, cdh, cdi , ceg, ceh, cei, cfg, cfh , cfi]
+        System. out.println(letterCombinations( "234"));
+    }
+
+    public static List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
 
         if (digits == null || digits.equals("")) {
             return result;
         }
 
-        Map<Character, char[]> map = new HashMap<Character, char[]>();
+        Map<Character, char[]> map = new HashMap<>();
         map.put('0', new char[] {});
         map.put('1', new char[] {});
         map.put('2', new char[] { 'a', 'b', 'c' });
@@ -34,7 +39,7 @@ public class LetterCombinationsofaPhoneNumber {
         return result;
     }
 
-    private void helper(Map<Character, char[]> map, String digits,
+    private static void helper(Map<Character, char[]> map, String digits,
                         StringBuilder sb, List<String> result) {
         if (sb.length() == digits.length()) {
             result.add(sb.toString());
