233b50505cfea1cdfcabaf7ff954e5bf9161c1cf
Yi Zhao <yizhao.cs@gmail.com>
2015-07-07 14:16:11 -0700

    1

---
+++ src/LinkedList/Advanced/Others/FindIntersectionOfTwoLists.java
@@ -0,0 +1,38 @@
+package LinkedList.Advanced.Others;
+
+import LibraryOfLeetcode.ListNode;
+
+/**
+ * Created by yizhao on 7/7/15.
+ */
+public class FindIntersectionOfTwoLists {
+    public static ListNode intersection(ListNode a, ListNode b) {
+        int aLength = getLength(a);
+        int bLength = getLength(b);
+
+        if (aLength > bLength) {
+            for (int i = 0; i < aLength - bLength; i++) {
+                a = a. next;
+            }
+        } else if (aLength < bLength) {
+            for (int i = 0; i < bLength - aLength; i++) {
+                b = b. next;
+            }
+        }
+
+        while (a != b && a != null && b != null) {
+            a = a. next;
+            b = b. next;
+        }
+        return a;
+    }
+
+    private static int getLength(ListNode cur){
+        int length = 0;
+        while (cur != null ) {
+            length++;
+            cur = cur. next;
+        }
+        return length;
+    }
+}
