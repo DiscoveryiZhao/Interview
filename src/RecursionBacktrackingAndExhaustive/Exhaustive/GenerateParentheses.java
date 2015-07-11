a9357f15431f6572a6712fd69e54902cb3be0a00
Yi Zhao <yizhao.cs@gmail.com>
2015-07-11 12:46:19 -0700

    1

---
+++ src/RecursionBacktrackingAndExhaustive/Exhaustive/GenerateParentheses.java
@@ -0,0 +1,37 @@
+package RecursionBacktrackingAndExhaustive.Exhaustive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by yizhao on 7/11/15.
+ */
+public class GenerateParentheses {
+    public static void main(String[] args) {
+        System.out.println(generateParenthesis(0)); // []
+        System.out.println(generateParenthesis(1)); // [()]
+        System.out.println(generateParenthesis(2)); // [(()), ()()]
+        System.out.println(generateParenthesis(3)); // [((())), (()()), (())(), ()(()), ()()()]
+    }
+
+    public static List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<>();
+        dfs(res, "", n, n);
+        return res;
+    }
+
+    private static void dfs(List<String> res, String tmp, int l, int r) {
+        if (l == 0 && r == 0) {
+            res.add(tmp);
+            return;
+        }
+        if (l > r)
+            return;
+        if (l > 0) {
+            dfs(res, tmp + "(", l - 1, r);
+        }
+        if (r > 0) {
+            dfs(res, tmp + ")", l, r - 1);
+        }
+    }
+}
