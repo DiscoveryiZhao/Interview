bdeaf5ad7b882d4fd96ac493937c74cf18868a1a
Yi Zhao <yizhao.cs@gmail.com>
2015-07-14 08:05:03 -0700

    1

---
+++ src/Trees/BinarySearchTreeAndBinaryTreeCross/ValidateBinarySearchTreeInorderStackTraversal.java
@@ -0,0 +1,33 @@
+package Trees.BinarySearchTreeAndBinaryTreeCross;
+
+import LibraryOfLeetcode.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * Created by yizhao on 7/14/15.
+ */
+public class ValidateBinarySearchTreeInorderStackTraversal {
+    public static boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true ;
+        } else {
+            TreeNode pre = null;
+            Stack<TreeNode> parentStack = new Stack<TreeNode>();
+            while (parentStack.size() != 0 || root != null) {
+                if (root != null) {
+                    parentStack.push(root);
+                    root = root. left;
+                } else {
+                    root = parentStack.pop();
+                    if (pre != null && pre.val >= root. val){
+                        return false ;
+                    }
+                    pre = root;
+                    root = root. right;
+                }
+            }
+            return true ;
+        }
+    }
+}
---
+++ src/Trees/BinarySearchTreeAndBinaryTreeCross/ValidateBinarySearchTreeRecursion.java
@@ -0,0 +1,23 @@
+package Trees.BinarySearchTreeAndBinaryTreeCross;
+
+import LibraryOfLeetcode.TreeNode;
+
+/**
+ * Created by yizhao on 7/14/15.
+ */
+public class ValidateBinarySearchTreeRecursion {
+    public static boolean isValidBST(TreeNode root) {
+        return dfs(root, Integer. MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    private static boolean dfs(TreeNode root, int low, int high) {
+        if (root == null) {
+            return true ;
+        } else {
+            if (root.val < low || root.val > high) {
+                return false ;
+            }
+            return dfs(root. left, low, root. val - 1) && dfs(root.right, root. val + 1, high);
+        }
+    }
+}
