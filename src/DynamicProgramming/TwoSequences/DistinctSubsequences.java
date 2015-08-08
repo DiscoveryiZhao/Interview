b7b39618f7e87a13a241314d6e366dbaf730fdc6
Yi Zhao <yizhao.cs@gmail.com>
2015-08-08 14:06:33 -0700

    1

--- src/DynamicProgramming/TwoSequences/DistinctSubsequences.java
+++ src/DynamicProgramming/TwoSequences/DistinctSubsequences.java
@@ -5,30 +5,32 @@ package DynamicProgramming.TwoSequences;
  */
 public class DistinctSubsequences {
     public static void main(String[] args) {
-        System. out.println(numDistinct( "abcde", "ace" ));// 1
-        System. out.println(numDistinct( "abcdce", "ace" )); // 2
-        System. out.println(numDistinct( "xabcde", "ace" ));// 1
-        System. out.println(numDistinct( "abcde", "xace" )); // 0
-        System. out.println(numDistinct( "rabbbit", "rabbit" )); // 3
+        System.out.println(numDistinct("abcde", "ace"));// 1
+        System.out.println(numDistinct("abcdce", "ace")); // 2
+        System.out.println(numDistinct("xabcde", "ace"));// 1
+        System.out.println(numDistinct("abcde", "xace")); // 0
+        System.out.println(numDistinct("rabbbit", "rabbit")); // 3
     }
 
     public static int numDistinct(String S, String T) {
         int m = S.length(), n = T.length();
-        // state:
+        // state: f[i][j] = S的前i个里面取出T的前j个字符，有多少种方案
         int[][] dp = new int[m + 1][n + 1];
-        // intialize for worst case:
-        dp[0][0] = 1; // Handles when both S == "" && T == ""
+
+        // intialize for worst case: Handles when T = ""
+        for (int i = 0; i < S.length(); i++) {
+            dp[i][0] = 1;
+        }
+
         // function:
-        for (int i = 0; i < m + 1; i++) {
-            for (int j = 0; j < n + 1; j++) {
-                if (i > 0 && j == 0) { // Handles when T = ""
-                    dp[i][0] = 1;
-                } else if (i > 0 && j > 0) {
-                    if (S.charAt(i - 1) == T.charAt(j - 1)) {
-                        dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
-                    } else {
-                        dp[i][j] = dp[i - 1][j];
-                    }
+        // f[i][j] = f[i-1][j] + f[i-1][j-1] when S[i-1] == T[j-1]
+        //         = f[i-1][j]               when S[i-1] != T[j-1]
+        for (int i = 1; i < m + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                if (S.charAt(i - 1) == T.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
