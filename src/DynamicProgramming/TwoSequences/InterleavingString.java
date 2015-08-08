79460f8989bc31a8075bf8ed73b84d56b0c987ae
Yi Zhao <yizhao.cs@gmail.com>
2015-08-08 14:29:30 -0700

    1

--- src/DynamicProgramming/TwoSequences/InterleavingString.java
+++ src/DynamicProgramming/TwoSequences/InterleavingString.java
@@ -17,7 +17,7 @@ public class InterleavingString {
         if (n1 + n2 != n3) {
             return false;
         }
-        // state:
+        // state: f[i][j]代表s1的前i个字符和s2的前j个字符能否交替组成s3的前i+j个字符
         boolean[][] dp = new boolean[n1 + 1][n2 + 1];
         // intialize for worst case: Handles when both s1 == "" && s2 == ""
         dp[0][0] = true;
@@ -31,19 +31,15 @@ public class InterleavingString {
 
         // intialize for worst case: Handles when s1 = ""
         for (int j = 1; j <= s2.length(); j++) {
-            if (s3.charAt(j - 1) == s2.charAt(j - 1) && dp[0][j - 1]){
+            if (s3.charAt(j - 1) == s2.charAt(j - 1) && dp[0][j - 1]) {
                 dp[0][j] = true;
             }
         }
 
-        // function:
+        // function: f[i][j] = f[i-1][j]&&(s1[i-1]==s3[i+j-1]) || f[i][j-1]&&(s2[j-1]==s3[i+j-1])
         for (int i = 1; i < n1 + 1; i++) {
             for (int j = 1; j < n2 + 1; j++) {
-                if(s1.charAt(i - 1) == s3.charAt(i - 1 + j) && dp[i - 1][j]){
-                    dp[i][j] = true;
-                }
-
-                if(s2.charAt(j - 1) == s3.charAt(i - 1 + j) && dp[i][j - 1]){
+                if ((s1.charAt(i - 1) == s3.charAt(i - 1 + j) && dp[i - 1][j]) || (s2.charAt(j - 1) == s3.charAt(i - 1 + j) && dp[i][j - 1])) {
                     dp[i][j] = true;
                 }
 //                dp[i][j] = s1.charAt(i - 1) == s3.charAt(i - 1 + j) && dp[i - 1][j] ? true : dp[i][j];
