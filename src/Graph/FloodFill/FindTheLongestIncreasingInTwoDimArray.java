1aed7d46afbc0bd43aaf82f7f9625efab82475a0
Yi Zhao <yizhao.cs@gmail.com>
2015-08-09 12:28:54 -0700

    1

--- src/Graph/FloodFill/FindTheLongestIncreasingInTwoDimArray.java
+++ src/Graph/FloodFill/FindTheLongestIncreasingInTwoDimArray.java
@@ -8,79 +8,50 @@ import java.util.List;
  */
 public class FindTheLongestIncreasingInTwoDimArray {
     public static void main(String[] args) {
-        int[][] board1 = { { 99, 88, 77, 66, 55 }, { 44, 4, 5, 33, 22 }, { 999, 5, 6, 7, 888 }, { 777, 6, 7, 8, 666 }, { 555, 444, 8, 9, 10 } };
-        int[][] board2 = { { 4, 3, 9 }, { 6, 5, 1 }, { 7, 8, 2 } };
-        int[][] board3 = { { 4, 3, 9 }, { 6, 5, 8 }, { 7, 6, 7 } };
-        System. out.println(solve(board1)); // [4, 5, 6, 7, 8, 9, 10]
-        System. out.println(solve(board2)); // [5, 6, 7, 8]
-        System. out.println(solve(board3)); // [5, 6, 7, 8, 9]
+        int[][] board1 = {{99, 88, 77, 66, 55}, {44, 4, 5, 33, 22}, {999, 5, 6, 7, 888}, {777, 6, 7, 8, 666}, {555, 444, 8, 9, 10}};
+        int[][] board2 = {{4, 3, 9}, {6, 5, 1}, {7, 8, 2}};
+        int[][] board3 = {{4, 3, 9}, {6, 5, 8}, {7, 6, 7}};
+        System.out.println(solve(board1)); // [4, 5, 6, 7, 8, 9, 10]
+        System.out.println(solve(board2)); // [5, 6, 7, 8]
+        System.out.println(solve(board3)); // [5, 6, 7, 8, 9]
     }
 
     public static List<Integer> res;
+
     public static List<Integer> solve(int[][] board) {
-        if (board.length == 0 || board[0].length == 0) { // P(0)
+        if (board.length == 0 || board[0].length == 0) {
             return new ArrayList<>();
         } else {
             res = new ArrayList<>();
-            boolean[][] visited = new boolean[board. length][board[0]. length];
+            boolean[][] visited = new boolean[board.length][board[0].length];
             for (int i = 0; i < board.length; i++) {
                 for (int j = 0; j < board[0].length; j++) {
-                    if (isValidForOne(board, visited, i, j)) {
-                        dfs(board, new ArrayList<Integer>(), visited, i, j);
-                    }
+                    dfs(board, new ArrayList<Integer>(), visited, board[i][j] - 1, i, j);
                 }
             }
-            return res ;
+            return res;
         }
     }
 
-    private static void dfs(int[][] board, List<Integer> tmp, boolean[][] visited, int i, int j) {
-        if(visited[i][j]){
+    private static void dfs(int[][] board, List<Integer> tmp, boolean[][] visited, int previousNum, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j]) {
             return;
         }
-        tmp.add(board[i][j]); // P(n)
-        visited[i][j] = true;
-        if (!isValidForOne(board, visited, i, j) && tmp.size() > res.size()) {
-            res = new ArrayList<Integer>(tmp);
+
+        if (board[i][j] == previousNum + 1) {
+            tmp.add(board[i][j]);
+            visited[i][j] = true;
+            if (tmp.size() > res.size()) {
+                res = new ArrayList<>(tmp);
+            }
+
+            dfs(board, tmp, visited, board[i][j], i - 1, j);
+            dfs(board, tmp, visited, board[i][j], i + 1, j);
+            dfs(board, tmp, visited, board[i][j], i, j - 1);
+            dfs(board, tmp, visited, board[i][j], i, j + 1);
+
+            tmp.remove(tmp.size() - 1);
+            visited[i][j] = false;
         }
-
-        if (isValidUp(board, visited, i, j)) {
-            dfs(board, tmp, visited, i - 1, j);// P(n+1)
-        }
-
-        if (isValidDown(board, visited, i, j)) {
-            dfs(board, tmp, visited, i + 1, j);// P(n+1)
-        }
-
-        if (isValidLeft(board, visited, i, j)) {
-            dfs(board, tmp, visited, i, j - 1);// P(n+1)
-        }
-
-        if (isValidRight(board, visited, i, j)) {
-            dfs(board, tmp, visited, i, j + 1);// P(n+1)
-        }
-
-        tmp.remove(tmp.size() - 1);
-        visited[i][j] = false;
-    }
-
-    public static boolean isValidForOne(int[][] board, boolean[][] visited, int i, int j) {
-        return isValidUp(board, visited, i, j) || isValidDown(board, visited, i, j) || isValidLeft(board, visited, i, j) || isValidRight(board, visited, i, j);
-    }
-
-    public static boolean isValidUp(int[][] board, boolean[][] visited, int i, int j) {
-        return (i - 1 >= 0 && board[i][j] + 1 == board[i - 1][j]);
-    }
-
-    public static boolean isValidDown(int[][] board, boolean[][] visited, int i, int j) {
-        return (i + 1 < board.length && board[i][j] + 1 == board[i + 1][j]);
-    }
-
-    public static boolean isValidLeft(int[][] board, boolean[][] visited, int i, int j) {
-        return (j - 1 >= 0 && board[i][j] + 1 == board[i][j - 1]);
-    }
-
-    public static boolean isValidRight(int[][] board, boolean[][] visited, int i, int j) {
-        return (j + 1 < board[0].length && board[i][j] + 1 == board[i][j + 1]);
     }
 }
