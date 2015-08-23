55a845a294c69654afbc3fa56a92f7bcbddfffbb
YI ZHAO <yizhao.cs@gmail.com>
2015-08-23 14:29:27 -0700

    1

--- src/SlidingWindow/UsingDeque/SlidingWindowMaximum.java
+++ src/SlidingWindow/UsingDeque/SlidingWindowMaximum.java
@@ -11,6 +11,33 @@ public class SlidingWindowMaximum {
         System.out.println(Arrays.toString(maxSlidingWindow(a, 3))); // [3, 3, 5, 5, 6, 7]
     }
 
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        if (k == 0) {
+            return new int[0];
+        }
+        Deque<Integer> deque = new LinkedList<>();
+        int i = 0;
+
+        for (int now : nums) {
+            i++;
+
+            while ((!deque.isEmpty() && now > deque.peekLast())) {
+                deque.pollLast();
+            }
+            deque.addLast(now);
+            if (i > k && deque.peekFirst() == nums[i - k - 1]) {
+                deque.pollFirst();
+            }
+            if (i > k - 1) {
+                res[i - k] = deque.peekFirst();
+            }
+            System.out.println("i:" + i + "  ,deque:" + deque);
+        }
+        return res;
+    }
+
 //    public static int[] maxSlidingWindow(int[] A, int w) {
 //        int n = A.length;
 //        Deque<Integer> leftDeque = new LinkedList<Integer>(); // deque stores index
@@ -42,30 +69,5 @@ public class SlidingWindowMaximum {
 //        return res;
 //    }
 
-    public static int[] maxSlidingWindow(int[] nums, int k) {
-        int n = nums.length;
-        int[] res = new int[n - k + 1];
-        if (k == 0) {
-            return new int[0];
-        }
-        Deque<Integer> deque = new LinkedList<>();
-        int i = 0;
 
-        for (int now : nums) {
-            i++;
-
-            while ((!deque.isEmpty() && now > deque.peekLast())) {
-                deque.pollLast();
-            }
-            deque.addLast(now);
-            if (i > k && deque.peekFirst() == nums[i - k - 1]) {
-                deque.pollFirst();
-            }
-            if (i > k - 1) {
-                res[i - k] = deque.peekFirst();
-            }
-            System.out.println("i:" + i + "  ,deque:" + deque);
-        }
-        return res;
-    }
 }
