87a81446de8d60b935e00f9baeff2d9bf9e17451
YI ZHAO <yizhao.cs@gmail.com>
2015-08-23 14:29:12 -0700

    1

--- src/SlidingWindow/UsingDeque/SlidingWindowMaximum.java
+++ src/SlidingWindow/UsingDeque/SlidingWindowMaximum.java
@@ -1,8 +1,6 @@
 package SlidingWindow.UsingDeque;
 
-import java.util.Arrays;
-import java.util.Deque;
-import java.util.LinkedList;
+import java.util.*;
 
 /**
  * Created by yizhao on 7/1/15.
@@ -13,33 +11,60 @@ public class SlidingWindowMaximum {
         System.out.println(Arrays.toString(maxSlidingWindow(a, 3))); // [3, 3, 5, 5, 6, 7]
     }
 
-    public static int[] maxSlidingWindow(int[] A, int w) {
-        int n = A.length;
-        Deque<Integer> leftDeque = new LinkedList<Integer>(); // deque stores index
-        int[] res = new int[n - w + 1];
-        int right = 0;
-        // Initialize deque for first window
-        while (right < w) {
-            // update deque for first window
-            while (!leftDeque.isEmpty() && A[leftDeque.getLast()] <= A[right]) {
-                leftDeque.removeLast();
-            }
-            leftDeque.addLast(right++);
-        }
+//    public static int[] maxSlidingWindow(int[] A, int w) {
+//        int n = A.length;
+//        Deque<Integer> leftDeque = new LinkedList<Integer>(); // deque stores index
+//        int[] res = new int[n - w + 1];
+//        int right = 0;
+//        // Initialize deque for first window
+//        while (right < w) {
+//            // update deque for first window
+//            while (!leftDeque.isEmpty() && A[leftDeque.getLast()] <= A[right]) {
+//                leftDeque.removeLast();
+//            }
+//            leftDeque.addLast(right++);
+//        }
+//
+//        res[right - w] = A[leftDeque.getFirst()]; // first window res at res[i-w]
+//        while (right < n) {
+//            // update deque for new window
+//            while (!leftDeque.isEmpty() && A[leftDeque.getLast()] <= A[right]) {
+//                leftDeque.removeLast();
+//            }
+//            leftDeque.addLast(right);
+//            // Pop older element outside window from deque
+//            while (leftDeque.getFirst() <= right - w) {
+//                leftDeque.removeFirst();
+//            }
+//            right++;
+//            res[right - w] = A[leftDeque.getFirst()];
+//        }
+//        return res;
+//    }
 
-        res[right - w] = A[leftDeque.getFirst()]; // first window res at res[i-w]
-        while (right < n) {
-            // update deque for new window
-            while (!leftDeque.isEmpty() && A[leftDeque.getLast()] <= A[right]) {
-                leftDeque.removeLast();
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        if (k == 0) {
+            return new int[0];
+        }
+        Deque<Integer> deque = new LinkedList<>();
+        int i = 0;
+
+        for (int now : nums) {
+            i++;
+
+            while ((!deque.isEmpty() && now > deque.peekLast())) {
+                deque.pollLast();
             }
-            leftDeque.addLast(right);
-            // Pop older element outside window from deque
-            while (leftDeque.getFirst() <= right - w) {
-                leftDeque.removeFirst();
+            deque.addLast(now);
+            if (i > k && deque.peekFirst() == nums[i - k - 1]) {
+                deque.pollFirst();
             }
-            right++;
-            res[right - w] = A[leftDeque.getFirst()];
+            if (i > k - 1) {
+                res[i - k] = deque.peekFirst();
+            }
+            System.out.println("i:" + i + "  ,deque:" + deque);
         }
         return res;
     }
