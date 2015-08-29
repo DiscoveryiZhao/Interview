532bccceac78055422b653efd36743bf3e58f773
YI ZHAO <yizhao.cs@gmail.com>
2015-08-29 11:47:04 -0700

    1

--- src/HashArrayTable/HashMap/RepeatedDNASequences.java
+++ src/HashArrayTable/HashMap/RepeatedDNASequences.java
@@ -5,14 +5,28 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Created by YZ on 8/29/15.
- */
+/*
+
+All DNA is composed of a series of nucleotides abbreviated as A, C, G, and T, for example: "ACGAATTCCG". When studying DNA, it is sometimes useful to identify repeated sequences within the DNA.
+
+Write a function to find all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule.
+
+For example,
+
+Given s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT",
+
+Return:
+["AAAAACCCCC", "CCCCCAAAAA"].
+*/
 public class RepeatedDNASequences {
     public static void main(String[] args){
         System.out.println(findRepeatedDnaSequences("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT")); // [AAAAACCCCC, CCCCCAAAAA]
     }
 
+
+    /*
+    * reference: http://blog.csdn.net/youmengjiuzhuiba/article/details/45768605
+    * */
     public static List<String> findRepeatedDnaSequences(String s) {
         List<String> rs = new LinkedList<>();
         Map<String, Integer> map = new HashMap<>();
