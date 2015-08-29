4199317069658edd446400a7120c4163ab6ea71e
YI ZHAO <yizhao.cs@gmail.com>
2015-08-28 20:32:32 -0700

    1

--- src/SelfTest/Test1.java
+++ src/SelfTest/Test1.java
@@ -4,50 +4,55 @@ package SelfTest;
  * Created by yizhao on 5/2/15.
  */
 public class Test1 {
-    // [2],
-// [3,4],
-// [6,5,7],
-// [4,1,8,3]
+
     public static void main(String[] args) {
-        String[] dict = {"dog", "cxat", "boy"};
-        System.out.println(sol(dict));
+        System.out.println(sol(4)); // 输出：8
     }
 
-
-    private static int sol(String[] dict){
-        String resultA = "";
-        String resultB = "";
-        int maxProductLength = 0;
-
-        for(int i = 0; i < dict.length; i++){
-            for(int j = i; j < dict.length; j++){
-                if(noCommon(dict[i],dict[j])){
-                    int tmpProductLength = dict[i].length()*dict[j].length();
-                    if(tmpProductLength >= maxProductLength){
-                        resultA = dict[i];
-                        resultB = dict[j];
-                        maxProductLength = tmpProductLength;
-                    }
-                }
-            }
+    public static int sol(int n) {
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return 2;
+        } else {
+            return sol(n - 1) * sol(n - 2);
         }
-        System.out.println("resultA:" + resultA + "  ,resultB:" + resultB);
-        return maxProductLength;
     }
 
-    private static boolean noCommon(String s, String t){
-        boolean[] hash = new boolean[256];
-        for(int i = 0; i < s.length(); i++){
-            if(!hash[s.charAt(i)]){
-                hash[s.charAt(i)] = true;
-            }
-        }
-
-        for(int i = 0; i < t.length(); i++){
-            if(hash[t.charAt(i)]){
-                return false;
-            }
-        }
-        return true;
-    }
+//    private static int sol(String[] dict){
+//        String resultA = "";
+//        String resultB = "";
+//        int maxProductLength = 0;
+//
+//        for(int i = 0; i < dict.length; i++){
+//            for(int j = i; j < dict.length; j++){
+//                if(noCommon(dict[i],dict[j])){
+//                    int tmpProductLength = dict[i].length()*dict[j].length();
+//                    if(tmpProductLength >= maxProductLength){
+//                        resultA = dict[i];
+//                        resultB = dict[j];
+//                        maxProductLength = tmpProductLength;
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println("resultA:" + resultA + "  ,resultB:" + resultB);
+//        return maxProductLength;
+//    }
+//
+//    private static boolean noCommon(String s, String t){
+//        boolean[] hash = new boolean[256];
+//        for(int i = 0; i < s.length(); i++){
+//            if(!hash[s.charAt(i)]){
+//                hash[s.charAt(i)] = true;
+//            }
+//        }
+//
+//        for(int i = 0; i < t.length(); i++){
+//            if(hash[t.charAt(i)]){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
 }
--- src/Trees/BinarySearchTree/InorderTraversal/FindKthSmallestInBST.java
+++ src/Trees/BinarySearchTree/InorderTraversal/FindKthSmallestInBST.java
@@ -33,24 +33,21 @@ public class FindKthSmallestInBST {
     * */
     public static int kthSmallest(TreeNode root, int k) {
         Stack<TreeNode> stack = new Stack<>();
-
-        TreeNode p = root;
         int result = 0;
-
-        while(!stack.isEmpty() || p!=null){
-            if(p!=null){
-                stack.push(p);
-                p = p.left;
+        while(!stack.isEmpty() || root!=null){
+            if(root!=null){
+                stack.push(root);
+                root = root.left;
             }else{
                 TreeNode t = stack.pop();
                 k--;
                 if(k==0) {
                     result = t.val;
+                    break;
                 }
-                p = t.right;
+                root = t.right;
             }
         }
-
         return result;
     }
 }
