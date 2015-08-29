0b5ad11a6f21c86e090e5fb59b375d4a1348fc62
YI ZHAO <yizhao.cs@gmail.com>
2015-08-28 17:29:57 -0700

    1

--- src/BruteForce/MinimumSizeSubarraySum.java
+++ src/BruteForce/MinimumSizeSubarraySum.java
@@ -1,20 +1,27 @@
 package BruteForce;
 
-/**
- * Created by YZ on 8/28/15.
- */
+/*      Given an array of n positive integers and a positive integer s, find the minimal length of a subarray of which the sum ≥ s. If there isn't one, return 0 instead.
+
+        For example, given the array [2,3,1,2,4,3] and s = 7,
+        the subarray [4,3] has the minimal length under the problem constraint.
+
+        click to show more practice.
+
+        More practice:
+        If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).*/
 public class MinimumSizeSubarraySum {
     /**
      * @param nums: an array of integers
-     * @param s: an integer
+     * @param s:    an integer
      * @return: an integer representing the minimum size of subarray
      */
     public int minSubArrayLen(int s, int[] nums) {
         int n = nums.length;
-        int i = 0;
-        int j = 0;
         int sum = 0;
         int result = Integer.MAX_VALUE;
+
+        int i = 0;
+        int j = 0;
         while (i < n) {
             while (j < n && sum < s) {
                 sum += nums[j];
