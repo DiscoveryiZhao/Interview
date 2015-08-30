978fc1b4e16c5a2897cad8e0e996d8c0187a2271
YI ZHAO <yizhao.cs@gmail.com>
2015-08-30 13:17:36 -0700

    1

--- src/DynamicProgramming/ForwardDpThenBackwardDp/TrappingRainWater.java
+++ src/DynamicProgramming/ForwardDpThenBackwardDp/TrappingRainWater.java
@@ -1,8 +1,11 @@
 package DynamicProgramming.ForwardDpThenBackwardDp;
+/*
 
-/**
- * Created by yizhao on 5/18/15.
- */
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it is able to trap after raining.
+
+For example,
+Given [0,1,0,2,1,0,1,3,2,1,2,1], return 6.
+*/
 public class TrappingRainWater {
     public static void main(String[] args) {
         int[] A = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
--- src/DynamicProgramming/MatrixDP/BinomialCoefficient/PascalTriangle.java
+++ src/DynamicProgramming/MatrixDP/BinomialCoefficient/PascalTriangle.java
@@ -1,36 +1,35 @@
 package DynamicProgramming.MatrixDP.BinomialCoefficient;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by yizhao on 5/18/15.
  */
 public class PascalTriangle {
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> result1 = generate(5);
         // [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
-        System. out.println(result1);
+        System.out.println(generate(5));
     }
 
-    public static ArrayList<ArrayList<Integer>> generate(int numRows) {
+    public static List<List<Integer>> generate(int numRows) {
         if (numRows == 0) {
-            return new ArrayList<ArrayList<Integer>>();
-        } else {
-            ArrayList<ArrayList<Integer>> res = new ArrayList<ArrayList<Integer>>();
-            int[] dp = new int[numRows + 1];
-            for (int i = 0; i < numRows; i++) {
-                ArrayList<Integer> row = new ArrayList<Integer>();
-                for (int j = i; j >= 0; j--) { // since triangle so we do reverse
-                    if (j == 0 || j == i) {
-                        dp[j] = 1;
-                    } else {
-                        dp[j] = dp[j - 1] + dp[j];
-                    }
-                    row.add(dp[j]);
-                }
-                res.add(row);
-            }
-            return res;
+            return new ArrayList<>();
         }
+        List<List<Integer>> res = new ArrayList<>();
+        int[] dp = new int[numRows + 1];
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = i; j >= 0; j--) { // since triangle so we do reverse
+                if (j == 0 || j == i) {
+                    dp[j] = 1;
+                } else {
+                    dp[j] = dp[j - 1] + dp[j];
+                }
+                row.add(dp[j]);
+            }
+            res.add(row);
+        }
+        return res;
     }
 }
