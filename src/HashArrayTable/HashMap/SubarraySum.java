b3c479046d2e9ebda2651896526712635ae32234
YI ZHAO <yizhao.cs@gmail.com>
2015-09-06 11:33:46 -0700

    1

--- src/HashArrayTable/HashMap/SubarraySum.java
+++ src/HashArrayTable/HashMap/SubarraySum.java
@@ -8,20 +8,22 @@ import java.util.Map;
  * Created by YZ on 9/6/15.
  */
 public class SubarraySum {
-    public ArrayList<Integer> subarraySum(int[] nums) {
+    public static void main(String[] args) {
+        int[] a = {-3, 1, 2, -3, 4};
+        System.out.println(subarraySum(a)); // [0, 2]
+        int[] b = {7, 1, 2, -3, 4};
+        System.out.println(subarraySum(b)); // [1, 3]
+    }
+
+    public static ArrayList<Integer> subarraySum(int[] nums) {
         // write your code here
-
         int n = nums.length;
-
         ArrayList<Integer> res = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
-
         map.put(0, -1);
-
         int sum = 0;
         for (int i = 0; i < n; i++) {
             sum += nums[i];
-
             if (map.containsKey(sum)) {
                 res.add(map.get(sum) + 1);
                 res.add(i);
