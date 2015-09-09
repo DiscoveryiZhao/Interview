1e3d701acebe0ebebc0ca582145b9b39d824ac19
YI ZHAO <yizhao.cs@gmail.com>
2015-09-08 18:17:31 -0700

    1

--- src/Trees/BinaryTree/Recursion_Traversal/BinaryTreePaths.java
+++ src/Trees/BinaryTree/Recursion_Traversal/BinaryTreePaths.java
@@ -4,10 +4,19 @@ import Library.Leetcode.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+/*
 
-/**
- * Created by YZ on 9/8/15.
- */
+Given a binary tree, return all root-to-leaf paths.
+
+For example, given the following binary tree:
+          1
+        /   \
+        2     3
+                \
+                5
+All root-to-leaf paths are:
+["1->2->5", "1->3"]
+*/
 public class BinaryTreePaths {
     public static void main(String[] args){
 
